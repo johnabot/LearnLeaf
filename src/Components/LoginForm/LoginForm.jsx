9fe4e3bacde015ebd2d2658036d66b39481f2910
Ibrahim Taha <itt4539@mavs.uta.edu>
2024-02-19 20:27:42 -0600

    added comments

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
 import './LoginForm.css';
 
+
+// LoginForm is a React component class that renders a login form.
 class LoginForm extends React.Component {
     render() {
       return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}>
-          <h1>LearnLeaf Task Organizer</h1>
+        /*Centering the login form to the center of the page
+        */
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', flexDirection: 'column' }}> 
+      
+          <h1>LearnLeaf Task Organizer</h1> 
+        
           <p>Helping you manage your everyday needs!</p>
           <form style={{ display: 'flex', flexDirection: 'column', width: '300px', alignItems: 'center' }}>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <label>
+            
+            <input type="text" placeholder="Username" /> 
+
+            <input type="password" placeholder="Password" /> 
+            
+            <label> 
+              
               <input type="checkbox" /> Remember me
+
             </label>
+
             <a href="/forgot-password">Forgot password?</a>
+
             <button type="submit">Login</button>
-            <p>Don't have an account? <a href="/register">Register</a></p>
-          </form>
+
+            <p>Don't have an account? <a href="/register">Register</a></p> 
+
+          </form> 
+            
         </div>
+        
       );
     }
   }
