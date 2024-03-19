07cff925cc49e992a19a76fe86d70cd8c5df0910
Elliot Mai <145508224+elliotmai@users.noreply.github.com>
2024-03-18 23:42:21 -0500

    resolve merge conflicts

--- src/LearnLeaf_Functions.js
+++ src/LearnLeaf_Functions.js
@@ -1,3 +1,4 @@
+
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import { initializeApp } from "firebase/app";
 // Add the Firebase products that you want to use
