51ffaf2c3e0bd382386eb6ccae162dcf1238a382
Elliot Mai <eli.mai12932@gmail.com>
2024-04-23 02:23:23 -0500

    Fix Image

--- src/Components/ProjectView/ProjectTasks.jsx
+++ src/Components/ProjectView/ProjectTasks.jsx
@@ -1,3 +1,4 @@
+import logo from '/src/LearnLeaf_Name_Logo_Wide.png';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useUser } from '/src/UserState.jsx';
